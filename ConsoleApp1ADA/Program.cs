875677a35e47d954b0c0503d6548d6a41e1e9341
NXTsk <mattejhorvath@gmail.com>
2021-09-08 11:27:08 +0200

    daco

--- ConsoleApp1ADA/Program.cs
+++ ConsoleApp1ADA/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1ADA
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Som hladný :-)");
         }
     }
 }
